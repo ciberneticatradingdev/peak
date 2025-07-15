695179c3582f960496b43035303145ecbc7be141
ciberneticatradingdev <ciberneticatrading@gmail.com>
2025-07-14 23:17:40 -0400

    first commit

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,3 +1,3 @@
-export const CA = "5Bin8bMu8Kd3h25thwGwbFDzRNr1nqfDD9yJAmhhbonk";
+export const CA = "9FzeRFVq5weQHTPRE8k1LA1etxU2TqA1vp71EBxYpump";
 export const x ='https://x.com/';
 export const tg = 'https://t.me/ZoomieOnSolPortal';
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,12 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			screens: {
+				'ipad-pro': '1024px', // iPad Pro específico
+				'desktop': '1280px', // Desktop real
+			},
+		},
 	},
 	plugins: [],
 }
