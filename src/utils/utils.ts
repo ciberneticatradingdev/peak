22c8266c94f2f0c1f625a1b1e04666299de35ac3
ciberneticatradingdev <ciberneticatrading@gmail.com>
2025-08-20 13:24:25 -0400

    asd

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,4 +1,4 @@
-export const CA = "CA WILL UPDATE NOW";
+export const CA = "A4L7eGDsxmH4AQHZxU4wDtZbEwbd7yxwoXsUXJcPpump";
 export const x = 'https://x.com/memestockmkt777';
 export const xcommunity = 'https://x.com/i/communities/1958195341427102137';
 export const tg = 'https://x.com/memestockmkt777';
