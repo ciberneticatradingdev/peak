733b70fbae3fd16a5a225f1238fe4580370454d7
ciberneticatradingdev <ciberneticatrading@gmail.com>
2025-08-19 21:50:26 -0400

    asd

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,3 +1,15 @@
-export const CA = "9FzeRFVq5weQHTPRE8k1LA1etxU2TqA1vp71EBxYpump";
-export const x ='https://x.com/';
-export const tg = 'https://t.me/ZoomieOnSolPortal';
+// Contract Address
+export const CA = "CA WILL UPDATE SOON";
+
+// Social Media Links
+export const TWITTER_URL = '';
+export const TIKTOK_URL = '';
+export const TELEGRAM_URL = '';
+
+// Trading Links
+export const PUMP_FUN_URL = `https://pump.fun/coin/${CA}`;
+export const DEXSCREENER_URL = `https://dexscreener.com/solana/${CA}`;
+
+// Legacy exports for backward compatibility
+export const x = TWITTER_URL;
+export const tg = TELEGRAM_URL;
