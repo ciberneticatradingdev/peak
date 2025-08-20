35f78f8cf02b0293756a8890c8709e78e0ca9bac
ciberneticatradingdev <ciberneticatrading@gmail.com>
2025-08-20 19:54:44 -0400

    asd

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,6 +1,6 @@
 export const CA = "CA WILL UPDATE SOON";
-export const x = 'https://x.com/';
-export const xcommunity = 'https://x.com/i/communities/';
+export const x = 'https://x.com/i/communities/1958315387528819098';
+export const xcommunity = 'https://x.com/i/communities/1958315387528819098';
 export const tg = 'https://x.com/';
 export const dex = `https://dexscreener.com/solana/${CA}`;
 export const raydium = `https://raydium.io/swap/?inputCurrency=sol&outputCurrency=${CA}&inputMint=sol&outputMint=${CA}`;
